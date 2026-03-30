59ae57af48f0e45db6b01f3413325277ffafc94c
Haiyang Hou <66674907+houhaiyang@users.noreply.github.com>
2026-03-30 16:20:55 +0800

    Update ESM-C.sh

--- repgene/getGeneEmbedding/Homo_sapien/ESM-C.sh
+++ repgene/getGeneEmbedding/Homo_sapien/ESM-C.sh
@@ -12,21 +12,21 @@
 # 加载系统 Conda
 source /home/HPCBase/tools/anaconda3/etc/profile.d/conda.sh
 # 加载环境配置
-source /home/share/huadjyin/home/houhaiyang/bashrc/ESM-C-py310-torch241-cu118.bashrc
+source /home/share/xxx/home/xxx/bashrc/ESM-C-py310-torch241-cu118.bashrc
 
-data_dir='/home/share/huadjyin/home/houhaiyang/project/RepGene/data/species/Homo_sapiens/'
+data_dir='/home/share/xxx/home/xxx/project/RepGene/data/species/Homo_sapiens/'
 
 # getEmbeddings
 cd ${data_dir}
 mkdir -p embeddings/proteins/
 # 使用 ESM-C 生成蛋白序列的 Embeddings
-python /home/share/huadjyin/home/houhaiyang/method/ESM-C/faa2emb_CLS-2.py \
+python /home/share/xxx/home/xxx/method/ESM-C/faa2emb_CLS-2.py \
     --input Homo_sapiens/ncbi_dataset/data/GCF_000001405.40/protein.faa \
     --outdir embeddings/proteins/
 
 # mergeEmbeddings
 cd ${data_dir}
-python /home/share/huadjyin/home/houhaiyang/method/ESM-C/merge_emb.py \
+python /home/share/xxx/home/xxx/method/ESM-C/merge_emb.py \
     --input Homo_sapiens/ncbi_dataset/data/GCF_000001405.40/protein.faa \
     --embeddings_dir embeddings/proteins/ \
     --output ./embeddings/proteins_merged_embeddings.csv.gz
